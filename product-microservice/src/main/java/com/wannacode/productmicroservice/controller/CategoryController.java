747ccbe583a390187b9e833313d3a8663727812a
toitoto <mazp05@gmail.com>
2023-11-04 23:02:18 -0500

    yy

--- product-microservice/src/main/java/com/wannacode/productmicroservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/wannacode/productmicroservice/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.wannacode.productmicroservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import lombok.Data;
 @RestController
 @RequestMapping("/api/categories")
 @Data
+@RefreshScope
 public class CategoryController {
 
 	@Value("${app.testProp}")
