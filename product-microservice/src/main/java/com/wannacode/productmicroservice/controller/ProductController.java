7d0dc5903d7d6a32cf60b9aac768aa114b8012ee
toitoto <mazp05@gmail.com>
2023-11-04 23:32:24 -0500

    yy

--- product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
+++ product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.wannacode.productmicroservice.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.wannacode.productmicroservice.repository.ProductRepository;
 
 @RestController
 @RequestMapping("/api/products")
+@RefreshScope
 public class ProductController {
 
 	@Autowired
