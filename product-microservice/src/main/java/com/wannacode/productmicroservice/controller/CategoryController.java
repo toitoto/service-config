8d99854b253348ca5e55888f79cfdb130bc0afbb
toitoto <mazp05@gmail.com>
2023-11-04 22:59:59 -0500

    ttt

--- product-microservice/src/main/java/com/wannacode/productmicroservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/wannacode/productmicroservice/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.wannacode.productmicroservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class CategoryController {
 	@Value("${app.testProp}")
 	private String testProp;
 
+	@GetMapping("test-prop")
 	public String getTestProp() {
 	return this.testProp;	
 	}
